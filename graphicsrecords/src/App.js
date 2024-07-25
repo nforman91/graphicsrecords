e35341af5ca0ef7f0c548103fc6800987ac6cc26
Nathan <nathan.forman@gmail.com>
2024-07-24 18:47:42 -0700

    Preparing page 5

--- graphicsrecords/src/App.js
+++ graphicsrecords/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import record from './img/record.jpg';
 import GlobalStyle from './components/GlobalStyle';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
-      <h1>Graphics Records</h1>
-      <img src={record}/>
+      <StyledHomePageTitle>Graphics Records</StyledHomePageTitle>
+      <StyledRecordImg src={record}/>
     </div>
   );
 }
+
+const StyledHomePageTitle = styled.h1`
+  margin: 2rem;
+`;
+
+const StyledRecordImg = styled.img`
+  margin-bottom: 2rem;
+`;
 
 export default App;
---
+++ graphicsrecords/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import {motion} from "framer-motion";
+// import {pageAnimation} from 
+
+// const Home = () => {
+//     return(
+
+//     );
+// }
+
+// export default Home;
