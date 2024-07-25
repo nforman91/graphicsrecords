315f7dff8d999de74501e401240589ed79e60984
Nathan <nathan.forman@gmail.com>
2024-07-24 17:48:00 -0700

    Preparing page 3

--- graphicsrecords/src/App.js
+++ graphicsrecords/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import record from './img/record.jpg';
+import GlobalStyle from './components/GlobalStyle';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <h1>Graphics Records</h1>
+      <img src={record}/>
     </div>
   );
 }
