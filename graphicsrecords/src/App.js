bb8b2ff02f05c38534a3e26b07ad4fbb90082f24
Nathan <nathan.forman@gmail.com>
2024-07-25 16:57:41 -0700

    Animation start 3

--- graphicsrecords/src/App.js
+++ graphicsrecords/src/App.js
@@ -17,7 +17,7 @@ function App() {
         {/* <Routes> */}
           {/* <Route exact path="/" element={<Home/>}/> */}
             <Home/>
-            <SpinningRecord/>
+            {/* <SpinningRecord/> */}
           {/* <Route exact path="/record">
             <StyledRecordImg src={record}/>
           </Route> */}
--- graphicsrecords/src/pages/Home.js
+++ graphicsrecords/src/pages/Home.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+// import SpinningRecord from "../components/SpinningRecord";
+import record from '../img/record.jpg';
 import {motion} from "framer-motion";
 import {pageAnimation} from "../animation";
 import styled from 'styled-components';
@@ -16,12 +18,22 @@ const Home = () => {
             exit="exit"
         >
             <StyledHomePageTitle>Graphics Records</StyledHomePageTitle>
+            {/* <SpinningRecord/> */}
+            <StyledRecordImg src={record}/>
         </motion.div>
     );
 }
 
 const StyledHomePageTitle = styled.h1`
   margin: 2rem;
+`;
+
+const StyledRecordImg = styled.img`
+  margin-bottom: 2rem;
+  border-radius: 50%;
+  /* hover: {
+    transform: rotate(45deg);
+  } */
 `;
 
 export default Home;
